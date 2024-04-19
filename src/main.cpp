832c153541c6188e4b5ac4f3e60a3c6a13d525d8
Krishna Kanhaiya <128268584+CaseyXD@users.noreply.github.com>
2024-04-19 12:37:51 +0800

    Wifi_connection_messege.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,3 +9,16 @@ const char* mqtt_server = "bdfc6e7f38564239bb84934a43c3abff.s1.eu.hivemq.cloud";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  // Connect to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  
+    }
+  }
+
