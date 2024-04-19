63f86209f0ad1c50636cde180c4300616639c0fa
newgede <162402769+newgede@users.noreply.github.com>
2024-04-19 13:14:45 +0800

    add_wifi_timeout_msg

--- src/main.cpp
+++ src/main.cpp
@@ -35,3 +35,19 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
