d7d97398aea9ac43d30d92de94bbfc551dd9e0c4
IndiraJayanti <165116625+IndiraJayanti@users.noreply.github.com>
2024-04-23 11:40:38 +0800

    Issue 2 Add Secret

--- MicroclimateAdjusterAgent/src/main.cpp
+++ MicroclimateAdjusterAgent/src/main.cpp
@@ -1,18 +1,82 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <secret.h>
 
-// put function declarations here:
-int myFunction(int, int);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  String topics = String(topic);
+  if(topics == "undiknas/FTI/TI/2022/IoT/Aditya/lampu"){
+    if(payload[0] == 1 ){
+      Serial.println("Nyalakan Lampu...");
+    }
+    else{
+      Serial.println("Matikan Lampu...");
+    }
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("Aditya123555777")) {
+      Serial.println("connected");
+      // Once connected, subscribe to the topic you wish to receive messages on
+      client.subscribe("undiknas/FTI/TI/2022/IoT/Aditya/#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  // Other loop code goes here
 }
