17598720f15f76ff71c5a30854479c1391f625bd
CaseyXD <krishnakanhaiyac@gmail.com>
2024-04-30 12:48:27 +0800

    Final Build (Working)

--- FertigationReservoirAgent/src/main.cpp
+++ FertigationReservoirAgent/src/main.cpp
@@ -21,7 +21,7 @@ void setup() {
   urusanWiFi.konek();
   urusanIoT.konek();
   urusanIoT.penangkapPesan(penangkapPesan);
-  urusanIoT.subscribe("tld/namaorganisasi/namadivisi");
+  urusanIoT.subscribe("org/elangnesia/FertigationReservoirAgent");
 
   penjadwal.init();
   penjadwal.addTask(task1);
@@ -49,6 +49,6 @@ void penangkapPesan(String topic, String message){
 /// @brief Fungsi callback dari task1
 void task1DetailTugas(){
   if(urusanIoT.apakahKonek() == 1){
-    urusanIoT.publish("tld/Rumah-Kaca-Elangnesia", "FertigationReservoirAgent");
+    urusanIoT.publish("org/elangnesia", "FertigationReservoirAgent");
   }
 }
