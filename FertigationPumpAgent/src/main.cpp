9b3c3678c20eb669e53fe5a8bb9e405fd9c1f981
purnasavitri <gekina01@gmail.com>
2024-05-14 12:14:10 +0800

    perubahan

--- FertigationPumpAgent/src/main.cpp
+++ FertigationPumpAgent/src/main.cpp
@@ -4,12 +4,16 @@
 #include <UrusanIoT.h>
 #include "secret.h"
 #include <TaskScheduler.h>
+#include <UrusanAktuatorReservoir.h>
+#include <ArduinoJson.h>
 
 void penangkapPesan(String topic, String message);
 void task1DetailTugas();
+void subscribeTopik();
 
 UrusanWiFi urusanWiFi(ssid, pass);
 UrusanIoT urusanIoT(broker, port, id, brokerUsername, brokerPassword);
+UrusanAktuatorReservoir urusanAktuatorReservoir(32);
 Scheduler penjadwal;
 
 Task task1(3000, TASK_FOREVER, &task1DetailTugas);
@@ -21,7 +25,12 @@ void setup() {
   urusanWiFi.konek();
   urusanIoT.konek();
   urusanIoT.penangkapPesan(penangkapPesan);
-  urusanIoT.subscribe("org/elangnesia/fertigationpumpagent");
+
+  if(urusanIoT.apakahKonek() == 1){
+    subscribeTopik();
+  }
+
+  urusanAktuatorReservoir.mulai();
 
   penjadwal.init();
   penjadwal.addTask(task1);
@@ -32,11 +41,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   urusanIoT.proses();
 
-  if(urusanWiFi.apakahKonek() == 1 && urusanIoT.apakahKonek() == 0){
+  if(urusanWiFi.apakahKonek() == true && urusanIoT.apakahKonek() == false){
     urusanIoT.konek();
+    if(urusanIoT.apakahKonek() == 1){
+      subscribeTopik();
+    }
   }
 
   penjadwal.execute();
+}
+
+void subscribeTopik(){
+  urusanIoT.subscribe("org/elangnesia/fertigationpumpagent/setelan");
 }
 
 /// @brief Fungsi callback dari fungsi subscribe objek urusanIoT
@@ -44,11 +60,37 @@ void loop() {
 /// @param message 
 void penangkapPesan(String topic, String message){
   Serial.printf("penangkapPesan: topic: %s | message: %s\n", topic.c_str(), message.c_str());
+    JsonDocument dataMasuk;
+    DeserializationError galatParseJson = deserializeJson(dataMasuk, message);
+    if(galatParseJson == DeserializationError::Ok){
+    if(dataMasuk["perintah"] != nullptr){
+      String perintah = dataMasuk["perintah"].as<String>();
+
+      if(perintah == String("nyalakan")){
+        urusanAktuatorReservoir.nyalakan();
+      }
+      else if(perintah == String("padamkan")){
+        urusanAktuatorReservoir.padamkan();
+      }
+    }
+    
+  }
+  else{
+    Serial.println("penangkapPesan: Format pesan tidak valid! Gunakan format JSON.");
+  }
 }
 
 /// @brief Fungsi callback dari task1
 void task1DetailTugas(){
-  if(urusanIoT.apakahKonek() == 1){
-    urusanIoT.publish("org/elangnesia", "fertigationpumpagent");
+  if(urusanIoT.apakahKonek() == true){
+    JsonDocument data;
+    char muatan[512];
+
+    data["status"] = urusanAktuatorReservoir.bacaStatus();
+
+    serializeJson(data, muatan);
+
+    urusanIoT.publish("org/elangnesia/fertigationpumpagent", muatan);
+  
   }
 }
